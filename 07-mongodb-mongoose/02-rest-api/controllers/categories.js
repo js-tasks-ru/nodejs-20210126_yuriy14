75f1303813289b209adbcaf086828adf6746d909
yarovikov <505-565@mail.ru>
2021-02-20 13:15:49 +0500

    add solution for 07-mongodb-mongoose 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,25 @@
+const Category = require('../models/Category');
+const {clearDocumentsHelper} = require('../helpers/clearDocumentsHelper');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const documents = await Category.find({});
+  
+  if (documents) {
+    const categories = clearDocumentsHelper(
+      documents, 
+      ['title'], 
+      [{
+        name: 'subcategories', 
+        columns: ['title']
+      }]
+    );
+
+    ctx.body = {categories};
+
+  } else {
+    ctx.throw(404, 'not found');
+  }
+    
 };
+
+
