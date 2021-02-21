975eafffd1bf9003ab5fbcf948edce628d748f96
yarovikov <505-565@mail.ru>
2021-02-21 13:59:08 +0500

    add solution for 08-authentication 01-local

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,26 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      let user;
+      
+      try {
+        user = await User.findOne({email});
+      } catch (error) {
+        console.error('БД не отвечает:', error.message);
+        return done(null, false, 'Неизвестная ошибка, повторите попытку позже.');
+      }
+      
+      if (! user) {
+        return done(null, false, 'Нет такого пользователя');
+      }
+
+      if (! await user.checkPassword(password)) {
+        return done(null, false, 'Неверный пароль');
+      }
+
+      done(null, user, 'Успешная авторизация!');
     },
 );
