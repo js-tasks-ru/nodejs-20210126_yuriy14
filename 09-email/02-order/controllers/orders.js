b29f64f651ecce95539dc68b6bf902cdcb0e3b8b
yarovikov <505-565@mail.ru>
2021-03-05 20:37:44 +0300

    add solution for 09-email 02-order

--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -2,7 +2,53 @@ const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+    const {product:productId, phone, address} = ctx.request.body;
+    const {user} = ctx;
+    let order;
+    let product;
+
+    try {
+        order = await Order.create({product:productId, phone, address, user});
+        order = await order.populate('product').execPopulate();
+        product = order.product;
+    } catch (err) {
+        if (err.name !== 'ValidationError') {
+            // console.log(err);
+            throw err;
+        }
+
+        const errors = {};
+
+        for (const field of Object.keys(err.errors)) {
+            errors[field] = err.errors[field].message;
+        }
+
+        // console.log(errors);
+
+        ctx.status = 400;
+        ctx.body = {errors};
+        return;
+    }
+
+    const {_id:orderId} = order; 
+
+    await sendMail({
+        template: 'order-confirmation',
+        to: user.email,
+        subject: 'Вы оформили заказ на learn.javascript.ru',
+        locals: {
+            id: orderId,
+            product
+        }
+    });
+
+    return ctx.body = {order: orderId};
+
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+    const {user} = ctx;
+    const orders = await Order.find({user});
+    ctx.body = {orders};
+    return;
 };
