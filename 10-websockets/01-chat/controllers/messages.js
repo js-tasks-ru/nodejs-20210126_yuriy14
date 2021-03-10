485cf696178a438f08735a7bb4104b8f0fec2ed4
yarovikov <505-565@mail.ru>
2021-03-10 20:24:39 +0500

    add solution for 10-websockets 01-chat

--- 10-websockets/01-chat/controllers/messages.js
+++ 10-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,18 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  const {_id: userId} = ctx.user;
+  
+  const rawMessages = await Message.find({chat: userId}).sort('-date').limit(20);
+  const messages = getMessages(rawMessages);
+
+  ctx.body = {messages};
+  return;
 };
+
+function getMessages (rawMessages) {
+  return rawMessages.map(msg => {
+    const {_id: id, date, text, user} = msg;
+    return {id, date, text, user};
+  });
+}
--- 10-websockets/01-chat/socket.js
+++ 10-websockets/01-chat/socket.js
@@ -6,12 +6,50 @@ const Message = require('./models/Message');
 function socket(server) {
   const io = socketIO(server);
 
-  io.use(async function(socket, next) {
-    next();
+  io.use(async function(socket, done) {
+    const {token} = socket.handshake.query;
+    
+    if (!token) {
+      return done(new Error('anonymous sessions are not allowed'));
+    }
+
+    let session;
+    
+    try {
+      session = await Session.findOne({token}).populate('user');
+    } catch (err) {
+      return done(new Error('service error'));
+    }
+
+    if (!session) {
+      return done(new Error('anonymous sessions are not allowed'));
+    }
+
+    socket.user = session.user;
+
+    done();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      if (!msg) {
+        return;
+      }
+
+      const {displayName, _id:userId} = socket.user;
+      
+      try {
+        await Message.create({
+          user: displayName,
+          chat: userId,
+          text: msg,
+          date: new Date()
+        });
+      } catch (err) {
+        console.log(err.message);
+      }
+      
+    });
   });
 
   return io;
